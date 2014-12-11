58e94f3de2529172bb037fe39d22ccb3f4c7a4dc
Keichi Takahashi <keichi.t@me.com>
2014-12-11 20:28:45 +0900

    UIを追加した

--- Hashi/AppDelegate.swift
+++ Hashi/AppDelegate.swift
@@ -13,6 +13,17 @@ import CoreBluetooth
 class AppDelegate: NSObject, NSApplicationDelegate, HashiCentralDelegate {
 
     @IBOutlet weak var window: NSWindow!
+    
+    @IBOutlet weak var lblAccX: NSTextField!
+    @IBOutlet weak var lblAccY: NSTextField!
+    @IBOutlet weak var lblAccZ: NSTextField!
+    
+    @IBOutlet weak var lblRotX: NSTextField!
+    @IBOutlet weak var lblRotY: NSTextField!
+    @IBOutlet weak var lblRotZ: NSTextField!
+    
+    @IBOutlet weak var lblConnectionStatus: NSTextField!
+    
     var hashiCentral: HashiCentral!
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
@@ -26,19 +37,29 @@ class AppDelegate: NSObject, NSApplicationDelegate, HashiCentralDelegate {
     }
 
     func didConnectToPeripheral(peripheral: CBPeripheral, error: NSError!) {
-        println("Connected to: \(peripheral.name)")
+        lblConnectionStatus.stringValue = "Connected"
+        lblConnectionStatus.textColor = NSColor.greenColor()
     }
     
     func didDisconnectFromPeripheral(peripheral: CBPeripheral, error: NSError!) {
-        println("Disconnected from: \(peripheral.name)")
+        lblConnectionStatus.stringValue = "Disconnected"
+        lblConnectionStatus.textColor = NSColor.redColor()
+    }
+    
+    private func formatFloatValue(value: Float) -> String {
+        return NSString(format: "%.3f", value)
     }
     
     func didUpdateAcceleration(accX: Float, accY: Float, accZ: Float) {
-        println("Acceleration: \(accX, accY, accZ)")
+        lblAccX.stringValue = formatFloatValue(accX)
+        lblAccY.stringValue = formatFloatValue(accY)
+        lblAccZ.stringValue = formatFloatValue(accZ)
     }
     
     func didUpdateRotationRate(rotX: Float, rotY: Float, rotZ: Float) {
-        println("Rotation Rate: \(rotX, rotY, rotZ)")
+        lblRotX.stringValue = formatFloatValue(rotX)
+        lblRotY.stringValue = formatFloatValue(rotY)
+        lblRotZ.stringValue = formatFloatValue(rotZ)
     }
 }
 
